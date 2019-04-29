de3b7f943cb400db0eaac8e4040565a2347122ae
Arthur Lapraye <arthur@lapraye.fr>
2019-04-29 18:01:54 +0200

    ajouts R script + TODO liste

--- statsdets.R
+++ statsdets.R
@@ -1,3 +1,18 @@
-#!/usr/bin/Rscript --vanilla 
+#!/usr/bin/Rscript --vanilla -i
+
+sfeaturefiles <- list.files(path="/home/arthur/programmes/python/stage/corpus/sfeatures/", pattern="*.txt", full.names=TRUE, recursive=FALSE)
+
+data=new.env()
+
+for (f in sfeaturefiles) { data[[ f ]] <- read.csv(f,header=TRUE,sep="\t") }
+
+for (n in names(data)) { }
 
 
+#lapply(files, function(x) {
+ #   t <- read.csv(x, header=TRUE,sep="\t") # load file
+    # apply function
+   
+    # write to file
+    #write.table(out, "path/to/output", sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+#})
