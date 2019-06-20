daa9ea5c25ec6db024d7e2a7fe26afe36c67261b
Arthur Lapraye <arthur@lapraye.fr>
2019-06-20 17:26:56 +0200

    clusterscript

--- sknclustering.py
+++ sknclustering.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 import sys
+import os
+
+import pandas as pd
 
 
 
 if __name__=="__main__":
 	for elem in sys.argv[1:]:
 		print(elem)
-	
+		x=pd.read_csv(elem,sep="\t")
+		
+		print(x.sum(axis=0,skipna = True)["word_n"] )
