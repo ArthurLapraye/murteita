b633c8747127b4332d8ffac1a165312af4d471e7
Arthur Lapraye <arthur@lapraye.fr>
2019-06-27 17:17:40 +0200

    c tout pr ajd

--- maptest.py
+++ maptest.py
@@ -2,4 +2,22 @@
 
 import folium
 
-dialectloc = folium.Map(location=[65,25],zoom_start=5,control_scale=True)
+import csv
+
+dialectloc = folium.Map(location=[65,25],zoom_start=6,control_scale=True)
+
+kyl=dict()
+
+with open("parishes") as fi:
+	for (ville,_,lat,lo) in csv.reader(fi):
+		if ville != "Nom":
+			print(ville)
+			kyl[ville]=(float(lat),float(lo))
+
+for elem in kyl:
+	folium.Marker(location=kyl[elem],
+	popup=elem,
+	icon=folium.Icon(color="red",icon="ok-sign")).add_to(dialectloc)
+	
+dialectloc.save("/tmp/sortie.html")
+		
